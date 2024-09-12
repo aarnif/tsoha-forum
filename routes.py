8f7fff4be847b857c118355cc81e508ff28b52a8
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-09-12 19:23:46 +0300

    Add user-, sub_forum-, thread-routes

--- routes.py
+++ routes.py
@@ -1,5 +1,37 @@
 from app import app
 
+
 @app.route("/")
 def index():
-    return "Hello World!"
+    return "The main page, NOT IMPLEMENTED YET!"
+
+# User routes
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return "User registration page, NOT IMPLEMENTED YET!"
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return "User login page, NOT IMPLEMENTED YET!"
+
+@app.route("/logout")
+def logout():
+    return "User logout, NOT IMPLEMENTED YET!"
+
+# Sub forum routes
+@app.route("/sub_forums")
+def sub_forums():
+    return "The sub-forums page, NOT IMPLEMENTED YET!"
+
+@app.route("/sub_forums/<int:sub_forum_id>")
+def sub_forum(sub_forum_id):
+    return f"The sub-forum {sub_forum_id}, NOT IMPLEMENTED YET!"
+
+# Thread routes
+@app.route("/sub_forums/<int:sub_forum_id>/threads")
+def threads(sub_forum_id):
+    return f"The sub-forum {sub_forum_id} threads, NOT IMPLEMENTED YET!"
+
+@app.route("/sub_forums/<int:sub_forum_id>/threads/<int:thread_id>")
+def thread(sub_forum_id, thread_id):
+    return f"The individual thread {thread_id} in sub-forum {sub_forum_id}, NOT IMPLEMENTED YET!"
