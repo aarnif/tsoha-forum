1289d6c5683e286647f3b4bbba608871b3160894
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-09-13 17:56:57 +0300

    Implement login and logout

--- routes.py
+++ routes.py
@@ -1,22 +1,36 @@
 from app import app
-
+from flask import render_template, request, session, redirect
+import users
 
 @app.route("/")
 def index():
-    return "The main page, NOT IMPLEMENTED YET!"
+    if session.get("username"):
+        return render_template("index.html")
+    
+    return redirect("/login")
 
 # User routes
 @app.route("/register", methods=["GET", "POST"])
 def register():
     return "User registration page, NOT IMPLEMENTED YET!"
 
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    return "User login page, NOT IMPLEMENTED YET!"
+@app.route("/login", methods=["GET"])
+def login_get():
+    return render_template("login.html")
+
+@app.route("/login", methods=["POST"])
+def login_post():
+    username = request.form["username"]
+    password = request.form["password"]
+    if users.check_credentials(username, password):
+        session["username"] = username
+
+    return redirect("/")
 
 @app.route("/logout")
 def logout():
-    return "User logout, NOT IMPLEMENTED YET!"
+    del session["username"]
+    return redirect("/login")
 
 # Sub forum routes
 @app.route("/sub_forums")
